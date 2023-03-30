8f7e0d2f9ee04917bfb84a0a69d52cad91050361
Oleh Skomorokha <oleg.skomoroha1@gmail.com>
2023-03-31 01:15:08 +0300

    win2 work need to change(something)

--- main.py
+++ main.py
@@ -26,62 +26,88 @@ def AboutMe():
 InfoB = Button(root, text='Виконав', command=AboutMe)
 InfoB.grid()
 # Задаємо елементи множин
-ListA = ['Катерина', 'Юлія', 'Дарина', 'Оксана', 'Ольга', 'Марія', 'Софія', 'Діана', 'Аліна']
-ListB = ['Віталій', 'Сергій', 'Олег', 'Петро', 'Василь', 'Федір', 'Богдан', 'Владислав', 'Віктор']
+set_A = ['Катерина', 'Юлія', 'Дарина', 'Оксана', 'Ольга', 'Марія', 'Софія', 'Діана', 'Аліна']
+set_B = ['Віталій', 'Сергій', 'Олег', 'Петро', 'Василь', 'Федір', 'Богдан', 'Владислав', 'Віктор']
 
 
 # вікно 2
 def window2():
-    root = Tk()
-    root.geometry("600x600")
-    root.title("win2")
+    win2 = Toplevel(root)
+    win2.title('Window 2')
     var = IntVar()
-    var.set(1)
-    # Creat and Pull LIST
-    boxA = Listbox(root, height=10, width=10, selectmode=MULTIPLE)
-    boxA.grid(row=1, column=0)
-    for i in ListA:
-        boxA.insert(END, i)
+    var.set(0)
+    a = set()
+    b = set()
 
-    boxB = Listbox(root, height=10, width=10, selectmode=MULTIPLE)
-    boxB.grid(row=1, column=1)
-    for i in ListB:
-        boxB.insert(END, i)
+    def add_women(event):
+        if var.get() == 0:
+            a.add(set_A[event.widget.curselection()[0]])
+        if var.get() == 1:
+            b.add(set_A[event.widget.curselection()[0]])
+        lbl_a['text'] = 'A = {}'.format(a)
+        lbl_b['text'] = 'B = {}'.format(b)
 
-
-    # Creat label and setA,B
-
-    def elA():
-        boxA['state'] = NORMAL
-        boxB['state'] = DISABLED
-
-    def elB():
-        boxA['state'] = DISABLED
-        boxB['state'] = NORMAL
+    def add_men(event):
+        if var.get() == 0:
+            a.add(set_B[event.widget.curselection()[0]])
+        if var.get() == 1:
+            b.add(set_B[event.widget.curselection()[0]])
+            lbl_b['text'] = 'B = {}'.format(b)
+            lbl_a['text'] = 'A = {}'.format(a)
 
     def save():
-        with open('resA.txt', 'a') as f:
-            f.write(str())
+        with open('result.txt', 'a') as f:
+            f.write(str(a))
             f.write('\n')
-            f.write(str())
+            f.write(str(b))
             f.write('\n')
-        Label(root, text='результати збережено в файл').grid(sticky=W, row=6, column=0)
+        save_btn.config(state=DISABLED)
 
+    def del_set():
+        a = set()
+        b = set()
+        lbl_a['text'] = 'A = {}'.format(a)
+        lbl_b['text'] = 'B = {}'.format(b)
+        save_btn.config(state=NORMAL)
+        with open('result.txt', 'w') as f:
+            f.write('')
 
-    #Save the result into file
-    saveA = Button(root, text='Записати результати в файл', command=save)
-    saveA.grid(sticky=W, row=5, column=0)
+    frm_win2 = Frame(win2, bg='red4', bd=10)
+    frm_win2.pack()
+    choose_set = Label(frm_win2, text='Оберіть до якої множини додавати елементи:', bg='red4',
+                            font=('Garamond', 14), fg='white')
+    choose_set.grid(row=0, columnspan=3)
+    radiobtn_A = Radiobutton(frm_win2, text='Множина А', font=('Garamond', 12), bg='red4',
+                                  activebackground='red4', variable=var, value=0)
+    radiobtn_A.grid(row=1, column=0)
+    radiobtn_B = Radiobutton(frm_win2, text='Множина B', font=('Garamond', 12), bg='red4',
+                                  activebackground='red4', variable=var, value=1)
+    radiobtn_B.grid(row=1, column=2)
+    lbl_fr1 = LabelFrame(frm_win2, text='Жіночі імена', font=('Garamond', 12), bg='red4', fg='white')
+    lbl_fr1.grid(row=2, column=0)
+    lst1 = Listbox(lbl_fr1, font=('Garamond', 14), selectmode=EXTENDED)
+    lst1.bind("<<ListboxSelect>>", add_women)
+    lst1.grid(row=2, column=0)
+    for i in set_A:
+        lst1.insert(END, i)
 
+    lbl_fr2 = LabelFrame(frm_win2, text='Чоловічі імена', font=('Garamond', 12), bg='red4', fg='white')
+    lbl_fr2.grid(row=2, column=2)
+    lst2 = Listbox(lbl_fr2, font=('Garamond', 14), selectmode=EXTENDED)
+    lst2.bind("<<ListboxSelect>>", add_men)
+    lst2.grid(row=2, column=2)
+    for i in set_B:
+        lst2.insert(END, i)
 
-    # radiobutton
-    rad1 = Radiobutton(root, text="Множина А", variable=var, value=0, command=elA)
-    rad1.grid(row=0, column=0)
-    rad2 = Radiobutton(root, text="Множина В", variable=var, value=1, command=elB)
-    rad2.grid(row=0, column=1)
-    root.configure(bg='#6E6E6E')
-    root.mainloop()
+    lbl_a = Label(frm_win2, text='A =', bg='red4', font=('Garamond', 14), fg='white')
+    lbl_a.grid(row=3, columnspan=3, sticky='w')
+    lbl_b = Label(frm_win2, text='B =', bg='red4', font=('Garamond', 14), fg='white')
+    lbl_b.grid(row=4, columnspan=3, sticky='w')
 
-
+    save_btn = Button(frm_win2, text='Зберегти множини', font=('Garamond', 12), bg='snow', command=save)
+    save_btn.grid(row=5, column=0)
+    del_btn = Button(frm_win2, text='Очистити множини', font=('Garamond', 12), bg='snow', command=del_set)
+    del_btn.grid(row=5, column=2)
 
 
 def window3():
@@ -103,6 +129,7 @@ def window3():
     setB.grid(row=1, column=1)
     for i in adB:
         setB.insert(END, i)
+
 
 # Створення вікон 2 3 4
 d = Menu(root)
